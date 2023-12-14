08d76bc264ed49d55e094a57bbaa58c38420f4df
e2350ric <145407765+e2350ric@users.noreply.github.com>
2023-12-14 15:35:04 +0800

    Update main5.c

--- EX2/main5.c
+++ EX2/main5.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(){
+    int i=10;
+    if(i<=30){
+        printf("免費");
+    }
+    else if(i>=240){
+        printf("240元");
+    }
+    else{
+        if(i%30){
+            int c=((i/30)+1)*30;
+            printf("%d元",c);
+            
+        }
+        else{
+            printf("%d元",i);
+    }
+}
+    return 0;
+}
