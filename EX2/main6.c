b194fbdefd0f1f0b82f05aacee20ff5cc9258fcf
e2350ric <145407765+e2350ric@users.noreply.github.com>
2023-12-14 15:45:02 +0800

    Update main6.c

--- EX2/main6.c
+++ EX2/main6.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int i=500;
+    if(i<=1500){
+        printf("70元");
+    }
+    else{
+        int n=i-1500;
+        if(n%100){
+            int c=((n/100)+1)*10;
+            printf("%d元",70+c);
+        }
+        else{
+            printf("%d元",70+(n/100)*10);
+        }
+    }
+    return 0;
+}
 
