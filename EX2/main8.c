da9478f203880d6949c86795733ff7405ad6c2bc
e2350ric <145407765+e2350ric@users.noreply.github.com>
2023-12-14 15:51:15 +0800

    Update main8.c

--- EX2/main8.c
+++ EX2/main8.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(){
+    int x=3;
+    int y=2;
+    int z=1;
+    if(x<0){
+        int s=(x*-1)*100+y*10+z;
+        printf("%d",s);
+    }
+    else{
+        printf("%d",x*100+y*10+z);
+    }
+    return 0;
+}
